c77d090b69a6b1a092e049e76620bccaff1f31a6
Kannan M <139197875+Kannan25897@users.noreply.github.com>
2023-09-07 07:42:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+// // import logo from './logo.svg';
+// import './child.css';
+//import Parent from './componets/Parent';
+
+import Child from './todo project/Child';
+
+// import  Intro from"./priya intro"
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello Developers</h1>
+//       <h2>Welcome</h2>
+//       <h3>To</h3>
+//       <Intro/>
+//       </div>)}
+        /* Learn React
+        </a>
+      </header>
+    </div>
+  );
+} */
+
+// function App() {
+//   return(
+
+// <>
+// <Parent/>
+// <h2> hello</h2>
+// </>
+//   )
+// }
+
+// function App(){
+//   const handleChild=() =>{
+//     console.log('hi');
+    
+//   }
+//   return(
+//     <>
+//     {/* <Child fun={handleChild}/ */}
+//     <Parent/>
+//     <Child/>
+    
+//     </>
+//   )
+// }
+
+function App(){
+  return(
+    <>
+      <Child/>
+    
+    </>
+  )
+}
+
+
+
+export default App;
